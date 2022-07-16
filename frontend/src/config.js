2fa635fd8c86642df9126ef76a889d0f076a67ba
cyadhunandan <109392082+cyadhunandan@users.noreply.github.com>
2022-07-16 12:44:51 -0400

    Update config.js
    Updated for Amazon host name

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,2 @@
-export const API_URL = 'http://ec2-18-212-206-82.compute-1.amazonaws.com:8080/'
+export const API_URL = 'http://ec2-35-172-118-10.compute-1.amazonaws.com:8080/'
 export default API_URL
